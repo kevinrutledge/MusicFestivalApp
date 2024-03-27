0eb23ab6d3b703bed90dfe8e748cf86c807294c6
jjustinn53 <jjustinn53@gmail.com>
2024-03-26 19:58:24 -0700

    Revert "reordered if statements to check if festival is null first"
    This reverts commit 65a40e37b25731f9bbddacbcbe930e58cb9398c8.

--- src/main/MusicFestival.java
+++ src/main/MusicFestival.java
@@ -208,12 +208,14 @@ public class MusicFestival {
                         Festival searchFestival = new Festival(line);
                         Festival toOrder = festivalsByName.search(searchFestival, NAME_COMPARATOR);
                         
-                         if (toOrder == null) {
-                            System.out.println("Invalid name");
-                            continue;
-                        } else if(toOrder.getTicketsRemaining() == 0) {
-                        	System.out.println("\n"+  toOrder.getName() + " is sold out. Please try again.\n");
+                        if(toOrder.getTicketsRemaining() == 0) {
+                        	System.out.println("\n"+  toOrder.getName() + "is sold out. Please try again.\n");
                         	continue;
+                        }
+                        
+                        if (toOrder == null) {
+                            System.out.println("Invalid name");
+                            break;
                         } else {
                             System.out.println("Festival details:");
                             System.out.println(toOrder.toString());
